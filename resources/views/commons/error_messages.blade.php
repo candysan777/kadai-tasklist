e9fcb196728432a77ff556c5079601e62316862f
candysan777 <learning123@gmail.com>
2022-02-04 12:14:03 +0000

    user registration

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -6,7 +6,7 @@
     </ul>
 @endif
 
-@if (count($errors) > 10)
+@if (count($errors) > 0)
     <ul class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
             <li class="ml-4">{{ $error }}</li>
